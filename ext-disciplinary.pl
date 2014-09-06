2f8d94e30457426b6e3a08b49371fe2f00553dda
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-09-06 17:43:22 +0900

    also discipline "まれいたそ"

--- ext-disciplinary.pl
+++ ext-disciplinary.pl
@@ -17,7 +17,7 @@ $handle=sub{
 
 	$dtext=&descape($ref->{'text'});
 	# return if the tweet does not contain jokes
-	return 0 if (($dtext !~ /COMA/i) && ($dtext !~ /C○MA/i) && ($dtext !~ /独楽/i));
+	return 0 if (($dtext !~ /COMA/i) && ($dtext !~ /C○MA/i) && ($dtext !~ /独楽/i) && ($dtext !~ /まれいたそ/i));
 
 	# disabled due to status duplicate issue
 	## do silent reply
